1c9c137015d1cf395a65b9342ce6c3ca9304cf67
Fathan Margono <fmgonoo@gmail.com>
2024-03-17 20:55:54 +0700

    initial project

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is CPM
+CPM, stand for Code Project Manager. Is an cross-platforms app to check how much project the dev have in their disk/machine and tells how much storage space is used.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>SvelteKit Starter Template</title>
+	<meta
+		name="description"
+		content="A starter template for SvelteKit w/ TypeScript, TailwindCSS pre-configured with ESLint, Prettier and Husky git hooks."
+	/>
+</svelte:head>
+
+<main class="min-h-screen w-full py-12 md:py-24 lg:py-32 xl:py-48">
+	<h1>Hello</h1>
+</main>
