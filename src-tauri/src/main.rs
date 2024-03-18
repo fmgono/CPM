625e049b0676cc584f8afa0802e02ebeb44662f8
Fathan Margono <fmgonoo@gmail.com>
2024-03-18 13:28:49 +0700

    feat: start writing rust function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::fs;
+use std::path::Path;
+use walkdir::WalkDir;
+
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![greet, get_all_projects])
+    // .invoke_handler(tauri::generate_handler![get_all_projects])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
@@ -12,4 +17,48 @@ fn main() {
 #[tauri::command]
 fn greet(name: &str) -> String {
   format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+fn get_all_projects() {
+  let start_dir = "/Users/fmgono/";
+  let mut project_names = vec![];
+
+  let walker = WalkDir::new(start_dir)
+    .into_iter()
+    .filter_entry(|e| e.file_name() != "node_modules");
+
+  for entry in walker.filter_map(|e| e.ok()) {
+    if entry.file_name() == "package.json" {
+      if let Some(parent) = entry.path().parent() {
+        if let Some(project_name) = parent.file_name() {
+          let size = get_directory_size(parent);
+          project_names.push((project_name.to_string_lossy().to_string(), size))
+        } else {
+          eprint!("Error: Could not get parent directory name")
+        }
+      }
+    } else {
+      eprint!("Error: Could not get parent directory")
+    }
+  }
+}
+
+fn get_directory_size(path: &Path) -> u64 {
+  let metadata = fs::metadata(path).unwrap();
+  if metadata.is_dir() {
+      let mut total_size = 0;
+      for entry in fs::read_dir(path).unwrap() {
+          let entry = entry.unwrap();
+          let file_type = entry.file_type().unwrap();
+          if file_type.is_file() {
+              total_size += entry.metadata().unwrap().len();
+          } else if file_type.is_dir() {
+              total_size += get_directory_size(&entry.path());
+          }
+      }
+      total_size
+  } else {
+      metadata.len()
+  }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
-	import Greet from "$components/greet.svelte";
+	import { invoke } from "@tauri-apps/api/tauri";
+
+	import { File } from "lucide-svelte";
+	import { Button } from "$lib/components/ui/button";
+	import { Card, CardDescription, CardHeader, CardTitle } from "$lib/components/ui/card";
+	import { Checkbox } from "$lib/components/ui/checkbox";
+	import {
+		Table,
+		TableBody,
+		TableCell,
+		TableHead,
+		TableHeader,
+		TableRow
+	} from "$lib/components/ui/table";
+	import { onMount } from "svelte";
+
+	let projects = [];
+	onMount(async () => {
+		// projects = await invoke("get_all_projects")
+	});
 </script>
 
 <svelte:head>
@@ -10,6 +29,65 @@
 	/>
 </svelte:head>
 
-<main class="min-h-screen w-full">
-	<Greet />
+<main class="m-8 flex min-h-screen w-full flex-col">
+	<!-- <section>
+    <h2 class="text-2xl font-semibold mb-4">Explore By Format</h2>
+    <div class="grid grid-cols-4 gap-4 mb-8">
+      <Card class="bg-[#fee2e2]">
+        <CardHeader>
+          <File />
+          <CardTitle>Audio Files</CardTitle>
+          <CardDescription>Size: 15 GB</CardDescription>
+        </CardHeader>
+      </Card>
+      <Card class="bg-[#e0e7ff]">
+        <CardHeader>
+          <File />
+          <CardTitle>Video Files</CardTitle>
+          <CardDescription>Size: 48 GB</CardDescription>
+        </CardHeader>
+      </Card>
+      <Card class="bg-[#e0e7ff]">
+        <CardHeader>
+          <File />
+          <CardTitle>PDF Files</CardTitle>
+          <CardDescription>Size: 10 GB</CardDescription>
+        </CardHeader>
+      </Card>
+      <Card class="bg-[#e0e7ff]">
+        <CardHeader>
+          <File />
+          <CardTitle>Zip Files</CardTitle>
+          <CardDescription>Size: 60 GB</CardDescription>
+        </CardHeader>
+      </Card>
+    </div>
+  </section> -->
+	<h3 class="mb-4 text-xl font-semibold">All Projects</h3>
+	<Table>
+		<TableHeader>
+			<TableRow>
+				<TableHead class="w-52">Name</TableHead>
+				<TableHead>Size</TableHead>
+				<TableHead>Last Modified</TableHead>
+				<TableHead>Last Opened</TableHead>
+			</TableRow>
+		</TableHeader>
+		<TableBody>
+			<TableRow>
+				<TableCell class="flex w-52 items-center gap-2 font-medium ">
+					<File class="w-[200px]" />
+					<p class="truncate">
+						Naval Ravikant Podcastasadsda Podcastasadsda Podcastasadsda Podcastasadsda
+						Podcastasadsda Podcastasadsda
+					</p>
+				</TableCell>
+				<TableCell>10 MB</TableCell>
+				<TableCell>10th Feb 2024</TableCell>
+				<TableCell>17th Feb 2024</TableCell>
+			</TableRow>
+		</TableBody>
+	</Table>
+
+	<pre>{JSON.stringify(projects)}</pre>
 </main>
